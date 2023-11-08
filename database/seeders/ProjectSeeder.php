21ce61d141b937e96ee0dd68aed799bbccd7789e
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-11-08 17:54:57 +0100

    Created resource Project withh all the necessary and connected to route

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 15; $i++) {
+            $project = new Project();
+            $project->title = $faker->realText(50);
+            $project->cover_image = 'placeholders/' . $faker->image('public/storage/placeholders', category: 'Projects', fullPath: false);
+            $project->slug = Str::slug($project->title, '-');
+            $project->description = $faker->realText();
+            $project->save();
+
+            # code...
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashBoardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +20,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/* ROTTE ADMIN */
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [ProjectController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+
+    /* all ruote start (admin.dashboard) or other */
+});
+
+
+
+
+/* ROTTE PROFILE */
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
